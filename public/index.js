23a6adae96abc18aa163da3dfaaa55db7c165092
Admin <admin@Admins-Mac-mini.local>
2019-06-30 17:10:05 -0700

    adds changes to frontend html

---
+++ public/index.js
@@ -0,0 +1,8 @@
+function studentClick(event){
+  event.preventDefault();
+  console.log('Student was Clicked');
+}
+function adminClick(event){
+  event.preventDefault();
+  console.log('Admin was Clicked');
+}
