48d1164fd1abf0781e9b9bff4cfbd9e08f9f5aef
Roy Hobbs <roy.hobbs@sdcs.io>
2019-06-30 17:10:30 -0700

    Implements group generating functionality

--- server/server.js
+++ server/server.js
@@ -1,72 +1,81 @@
 const express = require("express");
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 const students = [
-    {
-        name: 'student 1',
-        skill: 4,
-    },
-    {
-        name: 'student 2',
-        skill: 2,
-    },
-    {
-        name: 'student 3',
-        skill: 12,
-    },
-    {
-        name: 'student 4',
-        skill: 7,
-    },
-    {
-        name: 'student 5',
-        skill: 1,
-    },
-    {
-        name: 'student 6',
-        skill: 11,
-    }
+  {
+    name: "student 1",
+    skill: 4
+  },
+  {
+    name: "student 2",
+    skill: 2
+  },
+  {
+    name: "student 3",
+    skill: 12
+  },
+  {
+    name: "student 4",
+    skill: 7
+  },
+  {
+    name: "student 5",
+    skill: 1
+  },
+  {
+    name: "student 6",
+    skill: 11
+  }
 ];
 
 let options = {
-    students,
-    random: true,
-    size: 3
-}
+  students,
+  random: true,
+  size: 3
+};
 
 function generateGroups(students, random, size) {
-    if(random){
-        let temp = (students.length-1) / size;
-        // TODO: add logic to create random groups
-        return [[
-                {
-                    name: "tom",
-                    skill: 3
-                },
-                {
-                    name: "mary",
-                    skill: 11
-                },
-            ],
-            [
-                {
-                    name: "june",
-                    skill: 11
-                },
-                {
-                    name: "frank",
-                    skill: 1
-                },
-            ],
-        ]
+  if (random) {
+    let temp = (students.length - 1) / size;
+    // TODO: add logic to create random groups
+
+    // return random students;
+  }
+
+  students = students.sort((a, b) => a.skill - b.skill);
+
+  let buckets = [[]];
+  let numberOfGroups = Math.trunc(students.length / size);
+
+  for (let i = 0; i < numberOfGroups; i++) {
+    if (i === numberOfGroups - 1) {
+      buckets[i] = students.slice(i * numberOfGroups + 1);
+    } else
+      buckets[i] = students.slice(
+        i * numberOfGroups,
+        (i + 1) * numberOfGroups + 1
+      );
+    buckets[i].sort(function(a, b) {
+      return 0.5 - Math.random();
+    });
+  }
+  let results = [[]];
+  var n = 0;
+  while (buckets.length != 0) { //unfinished while loop
+    for (let i = 0; i < buckets.length; i++) {
+      if (results[n] == undefined) results[n] = [buckets[i].pop()];
+      else results[n] = results[n].push(buckets[i].pop());
     }
+    n++;
+  }
+  return results;
 }
 
 app.post("/admin", (req, res) => {
@@ -74,13 +83,13 @@ app.post("/admin", (req, res) => {
   res.json(generateGroups(students, random, size));
 });
 
-app.post("/add-student", (req, res)=> {
+app.post("/add-student", (req, res) => {
   students.push(req.body);
 });
 
 // Currently sending back sorted students array to root in ascending order
-app.get('/', function(req, res){
-  students.sort((a, b) => (a.skill - b.skill));
+app.get("/", function(req, res) {
+  students.sort((a, b) => a.skill - b.skill);
   res.json(students);
 });
 
