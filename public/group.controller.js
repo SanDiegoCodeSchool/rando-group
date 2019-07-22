7c4f89581270bc2f71d88d764e2d9b5ec8deaa7f
Michael Roberts <michael@origincodeacademy.com>
2019-07-21 18:01:54 -0700

    fixes tables so that they dynamically display per users

--- public/group.controller.js
+++ public/group.controller.js
@@ -1,45 +1,26 @@
-const mockData = [
-    {
-      name: "student 1",
-      skill: 4
-    },
-    {
-      name: "student 2",
-      skill: 2
-    },
-    {
-      name: "student 3",
-      skill: 12
-    },
-    {
-      name: "student 4",
-      skill: 7
-    },
-    {
-      name: "student 5",
-      skill: 1
-    },
-    {
-      name: "student 6",
-      skill: 11
-    }
-  ];
-
-  function createTable(mockData){
-    var table = document.getElementsByTagName('table');
-
-    var tableBody = document.createElement('tbody');
-    var td = document.createElement('td');
-    var tr = document.createElement('tr');
-    
-    mockData.map(function (student){
-    td.appendChild(document.createTextNode(`${student.name}`))
-    tr.appendChild(td)
-
+  fetch('/group')
+    .then(res => res.json())
+    .then((result)=> {
+      result.map(group => createTable(group));
     })
-    
-    tableBody.appendChild(tr);
-    table.appendChild(tableBody)
-    }
+    .catch(err => console.log('feels bad no data'));
 
- createTable(mockData);
+function createTable(students){
+  const table = document.createElement('table');
+
+  const tableBody = document.createElement('tbody');
+  let tr;
+  let td;
+  
+  students.map(function (student){
+    tr = document.createElement('tr');
+    td = document.createElement('td');
+
+    td.appendChild(document.createTextNode(`(${student.skill}) ${student.name} `));
+    tr.appendChild(td);
+    tableBody.appendChild(tr);
+  });
+  
+  table.appendChild(tableBody)
+  document.body.appendChild(table);
+}
